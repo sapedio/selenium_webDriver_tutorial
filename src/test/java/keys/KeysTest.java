a584a7858502a64395ea9006f67ae5e8753bd501
Simon Mosqueda <simonmosqueda@gmail.com>
2024-06-20 12:36:33 -0500

    First iteration of selenium_WebDriver class 20-06-2024

--- src/test/java/keys/KeysTest.java
+++ src/test/java/keys/KeysTest.java
@@ -12,7 +12,7 @@ public class KeysTest extends BaseTest {
     public void testBackspace(){
         var keyPage = homePage.clickKeyPresses();
         keyPage.enterText("A" + Keys.BACK_SPACE);
-        assertEquals(keyPage.getResult(),"You entered: BACK_SPACE");
+        assertEquals(keyPage.getResult(),"You entered: BACK_SPACE!");
     }
 
     @Test
